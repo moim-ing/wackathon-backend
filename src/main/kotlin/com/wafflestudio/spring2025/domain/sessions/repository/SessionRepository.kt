29dd5438aa5ffc3bed08f4e2ce5464e573578b32
uykhc <yoon002003@snu.ac.kr>
2026-02-22 04:15:46 +0900

    fix: api

--- src/main/kotlin/com/wafflestudio/spring2025/domain/sessions/repository/SessionRepository.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/domain/sessions/repository/SessionRepository.kt
@@ -4,5 +4,6 @@ import com.wafflestudio.spring2025.domain.sessions.entity.Session
 import org.springframework.data.repository.CrudRepository
 
 interface SessionRepository : CrudRepository<Session, Long> {
+    fun findByClassId(classId: Long): List<Session>
     fun findByVideoId(videoId: String): Session?
 }
