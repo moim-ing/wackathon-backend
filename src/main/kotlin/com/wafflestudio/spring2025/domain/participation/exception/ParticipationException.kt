133ed61969a2218f52633ff07b4d1c4ca95e8afa
uykhc <yoon002003@snu.ac.kr>
2026-02-22 04:09:22 +0900

    fix: api

--- src/main/kotlin/com/wafflestudio/spring2025/domain/participation/exception/ParticipationException.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/domain/participation/exception/ParticipationException.kt
@@ -6,22 +6,19 @@ open class ParticipationException(
     error: ParticipationErrorCode,
     cause: Throwable? = null,
 ) : DomainException(
-    httpErrorCode = error.httpStatusCode,
-    code = error,
-    title = error.title,
-    msg = error.message,
-    cause = cause,
-)
+        httpErrorCode = error.httpStatusCode,
+        code = error,
+        title = error.title,
+        msg = error.message,
+        cause = cause,
+    )
 
 class ParticipationValidationException(
     error: ParticipationErrorCode,
 ) : ParticipationException(error = error)
 
-class BannedUserCannotRegisterException :
-    ParticipationException(error = ParticipationErrorCode.BANNED_USER_CANNOT_REGISTER)
+class BannedUserCannotRegisterException : ParticipationException(error = ParticipationErrorCode.BANNED_USER_CANNOT_REGISTER)
 
-class SessionNotFoundException :
-    ParticipationException(error = ParticipationErrorCode.SESSION_NOT_FOUND)
+class SessionNotFoundException : ParticipationException(error = ParticipationErrorCode.SESSION_NOT_FOUND)
 
-class ClassNotFoundException :
-    ParticipationException(error = ParticipationErrorCode.CLASS_NOT_FOUND)
+class ClassNotFoundException : ParticipationException(error = ParticipationErrorCode.CLASS_NOT_FOUND)
