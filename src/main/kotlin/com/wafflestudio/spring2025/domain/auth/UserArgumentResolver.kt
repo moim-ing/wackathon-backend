c06e42add31e61528d38e0a45451a1ac630ac023
Jisu Hong <jaylovegood@snu.ac.kr>
2026-02-22 09:19:25 +0900

    debug

--- src/main/kotlin/com/wafflestudio/spring2025/domain/auth/UserArgumentResolver.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/domain/auth/UserArgumentResolver.kt
@@ -3,6 +3,7 @@ package com.wafflestudio.spring2025.domain.auth
 import com.wafflestudio.spring2025.domain.auth.exception.AuthenticationRequiredException
 import com.wafflestudio.spring2025.domain.user.model.User
 import com.wafflestudio.spring2025.domain.user.repository.UserRepository
+import kotlin.reflect.jvm.kotlinFunction
 import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.support.WebDataBinderFactory
@@ -24,7 +25,13 @@ class UserArgumentResolver(
         binderFactory: WebDataBinderFactory?,
     ): User? {
         val userId = webRequest.getAttribute("userId", 0) as? Long ?: return null
-        return userRepository.findById(userId)
-            .orElseThrow { AuthenticationRequiredException() }
+        val kotlinParam = parameter.kotlinFunction?.parameters
+            ?.getOrNull(parameter.parameterIndex + 1)
+        val isNullable = kotlinParam?.type?.isMarkedNullable == true
+        return if (isNullable) {
+            userRepository.findById(userId).orElse(null)
+        } else {
+            userRepository.findById(userId).orElseThrow { AuthenticationRequiredException() }
+        }
     }
 }
